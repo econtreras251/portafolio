20de988d47978ec9c4e44220c5907f0c151b375f
Edgar Contreras <ec542498@gmail.com>
2018-03-23 14:04:46 -0430

    Se aÃade la carga de imagenes y datos del About
    Se analiza la data, y se crea el metodo que permite a traves de un
    servicio, llamar los datos del about, con sus datos respectivos

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,16 +6,31 @@ export class InformacionService {
 
   info:any = {}
   cargada:boolean = false
+  firebase:boolean = false
+  equipo:any[] = []
 
   constructor( public http:Http ) {
 
+    this.carga_info()
+    this.carga_sobre_nosotros()
+
+  }
+
+  public carga_info(){
     this.http.get("assets/data/info.pagina.json")
               .subscribe( data=>{
-                console.log(data.json())
                 this.cargada = true
                 this.info = data.json()
               })
+  }
 
+  public carga_sobre_nosotros(){
+    this.http.get("https://angular-4f93c.firebaseio.com/equipo.json")
+              .subscribe( data=>{
+                console.log(data.json())
+                this.firebase = true
+                this.equipo = data.json()
+              })
   }
 
 }
