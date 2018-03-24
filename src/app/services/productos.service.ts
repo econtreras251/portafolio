915362608c46caac52395ad89f34c64e66f74c3b
Edgar Contreras <ec542498@gmail.com>
2018-03-24 10:59:04 -0430

    Agregando la busqueda

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,7 +5,9 @@ import { Http } from '@angular/http';
 export class ProductosService {
 
   productos:any[] = []
+  productos_filtrados:any[] = []
   cargando:boolean = true
+
 
   constructor( private http:Http ) {
     this.cargar_productos()
@@ -15,17 +17,61 @@ export class ProductosService {
     return this.http.get(`https://angular-4f93c.firebaseio.com/productos/${ cod }.json`)
   }
 
+  public buscar_producto( termino:string ){
+
+    if( this.productos.length === 0){
+      this.cargar_productos().then( ()=>{
+        //termino la carga
+      this.filtar_productos( termino )
+      })
+    }else{
+      this.filtar_productos( termino )
+    }
+
+  }
+
+
+  private filtar_productos( termino:string ){
+
+    this.productos_filtrados = []
+
+    termino = termino.toLowerCase()
+
+    this.productos.forEach( prod=>{
+
+      if( prod.categoria.indexOf( termino ) >= 0 || prod.titulo.toLowerCase().indexOf( termino ) >= 0){
+        this.productos_filtrados.push( prod )
+        console.log( prod )
+
+      }
+    })
+
+  }
+
+
+
+
+
   public cargar_productos(){
-    this.http.get('https://angular-4f93c.firebaseio.com/productos_idx.json')
-        .subscribe( res=>{
-          //console.log( res.json() )
 
-          //setTimeout( ()=>{
+    let promesa = new Promise( ( resolve, reject )=>{
 
-            this.cargando = false
-            this.productos = res.json()
-          //}, 1500)
-        })
+
+      this.http.get('https://angular-4f93c.firebaseio.com/productos_idx.json')
+      .subscribe( res=>{
+        //console.log( res.json() )
+
+        //setTimeout( ()=>{
+
+        this.cargando = false
+        this.productos = res.json()
+        resolve()
+        //}, 1500)
+      })
+
+
+    })
+    return promesa
   }
 
 
